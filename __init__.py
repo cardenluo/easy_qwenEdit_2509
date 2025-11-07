5dd310d7eb72bee3f19d965a68926c90853bfc0a
cardenluo <133877069+cardenluo@users.noreply.github.com>
2025-11-07 18:44:26 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+WEB_DIRECTORY = "./web"
+
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY", ]
+
+
+
+from .easy_qwen_edit import *
+
+
+NODE_CLASS_MAPPINGS = {
+    "Easy_QwenEdit2509": Easy_QwenEdit2509,
+
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Easy_QwenEdit2509": Easy_QwenEdit2509,
+}
 
